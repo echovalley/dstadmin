681f477215ff1caf2852c811914dffee14a81e97
tony <ramon_tong@126.com>
2012-09-26 16:54:05 +0800

    webstatistics

--- app/models/website_statistics.rb
+++ app/models/website_statistics.rb
@@ -1,11 +1,8 @@
-class WebsiteStatistics
+class WebsiteStatistics < ActiveRecord::Base
   attr_accessible :click, :impression, :income, :log_date
   belongs_to :website
 
   def self.search_by_website(website_id)
     statistics = SpotStatistics.search(:start_date => '2012-8-1', :website_id => website_id)
-    statistics.each do |t|
-
-    end
   end
 end
