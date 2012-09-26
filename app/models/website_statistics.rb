1cfcc1d0451db77840c63918bf31fb533c6f1fa4
tony <ramon_tong@126.com>
2012-09-26 16:48:34 +0800

    bug

--- app/models/website_statistics.rb
+++ app/models/website_statistics.rb
@@ -2,7 +2,7 @@ class WebsiteStatistics
   attr_accessible :click, :impression, :income, :log_date
   belongs_to :website
 
-  def self search_by_website(website_id)
+  def self.search_by_website(website_id)
     statistics = SpotStatistics.search(:start_date => '2012-8-1', :website_id => website_id)
     statistics.each do |t|
 
