67a01aaa5d23fb2c464b5e1aa386fbc62d57a9a2
tony <ramon_tong@126.com>
2012-10-12 16:39:38 +0800

    bundle config build.pg

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,6 @@
 #server "my_fancy_server.com", :app, :web, :db, :primary => true
 set :deploy_to, "/var/www/#{application}_staging"
+
+before "bundle:install" do
+  run "cd #{fetch(:latest_release)} && bundle config build.pg --with-pg=/usr/pgsql-9.2"
+end
